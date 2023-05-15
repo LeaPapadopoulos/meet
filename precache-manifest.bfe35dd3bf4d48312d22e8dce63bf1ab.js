5b138621fec4449c141d029f56b94813f115a68b
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-05-15 21:48:48 +0200

    Updates

---
+++ precache-manifest.bfe35dd3bf4d48312d22e8dce63bf1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d994601771bfe3455fc",
+    "url": "/meet/static/css/main.45196d28.chunk.css"
+  },
+  {
+    "revision": "4d994601771bfe3455fc",
+    "url": "/meet/static/js/main.4d994601.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/meet/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/meet/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "a28097751adba4319c06",
+    "url": "/meet/static/js/runtime~main.a2809775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6ea9980ddf683161c24ee04b8d3c3fa3",
+    "url": "/meet/index.html"
+  }
+];
