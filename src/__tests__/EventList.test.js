a301fd7497865d29e2903b3e0228e62e2a9c98fc
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-05-18 00:17:40 +0200

    Add Unit tests fro scenario 3 and use of mockData

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import EventList from "../EventList";
 import Event from "../Event";
+import { mockData } from "../mock-data";
 
 describe("<EventList /> component", () => {
   test("render correct number of events", () => {
@@ -9,5 +10,10 @@ describe("<EventList /> component", () => {
       <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
     );
     expect(EventListWrapper.find(Event)).toHaveLength(4);
+  });
+
+  test("render correct number of events", () => {
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
